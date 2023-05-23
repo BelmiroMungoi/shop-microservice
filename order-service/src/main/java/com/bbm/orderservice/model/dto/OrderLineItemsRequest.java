5348447e8bfa8d20fc71d7d868d245a1f0e90a23
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-05-23 16:11:53 +0200

    The orders requests to receive the data from client side

---
+++ order-service/src/main/java/com/bbm/orderservice/model/dto/OrderLineItemsRequest.java
@@ -0,0 +1,20 @@
+package com.bbm.orderservice.model.dto;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderLineItemsRequest {
+
+	private Long id;
+	private String skuCode;
+	private BigDecimal price;
+	private Integer quantity;
+}
