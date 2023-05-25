7d5cef084e875209966464eaca452e335271ed8a
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-05-25 13:14:43 +0200

    Changing the method to return a list of inventorys in stock

--- inventory-service/src/main/java/com/bbm/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/bbm/inventoryservice/service/InventoryService.java
@@ -1,11 +1,15 @@
 package com.bbm.inventoryservice.service;
 
+import com.bbm.inventoryservice.model.Inventory;
+import com.bbm.inventoryservice.model.dto.InventoryResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bbm.inventoryservice.repository.InventoryRepository;
 
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,7 +18,13 @@ public class InventoryService {
 	private final InventoryRepository inventoryRepository;
 	
 	@Transactional(readOnly = true)
-	public boolean isInStock(String skuCode) {
-		return inventoryRepository.findBySkuCode(skuCode).isPresent();
+	public List<InventoryResponse> isInStock(List<String> skuCode) {
+		return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+				.map(inventory ->
+						InventoryResponse.builder()
+								.skuCode(inventory.getSkuCode())
+								.isInStock(inventory.getQuantity() > 0)
+								.build()
+				).toList();
 	}
 }
