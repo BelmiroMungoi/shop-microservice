874a5f93d7408d0e71721f6d8d5b92ed13525de2
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-05-25 13:20:07 +0200

    Changing the searching method type

--- inventory-service/src/main/java/com/bbm/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/bbm/inventoryservice/repository/InventoryRepository.java
@@ -1,12 +1,14 @@
 package com.bbm.inventoryservice.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import com.fasterxml.jackson.databind.introspect.AnnotationCollector;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bbm.inventoryservice.model.Inventory;
 
 public interface InventoryRepository extends JpaRepository<Inventory, Long> {
 
-	Optional<Inventory> findBySkuCode(String skuCode);
+	List<Inventory> findBySkuCodeIn(List<String> skuCode);
 }
