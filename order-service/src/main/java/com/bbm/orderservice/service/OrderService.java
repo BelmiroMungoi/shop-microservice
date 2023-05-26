e80c8257dc6003bb1b66c8f2647df3eef0045c67
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-05-26 18:12:14 +0200

    Updating the uri for inventory service

--- order-service/src/main/java/com/bbm/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/bbm/orderservice/service/OrderService.java
@@ -24,7 +24,7 @@ public class OrderService {
 
 	private final OrderRepository orderRepository;
 
-	private final WebClient webClient;
+	private final WebClient.Builder webClientBuilder;
 	
 	public void placeOrder(OrderRequest orderRequest) {
 		Order order = new Order();
@@ -41,8 +41,8 @@ public class OrderService {
 				.map(OrderLineItems::getSkuCode).toList();
 
 		//Call the Inventory Service and place order if product is in stock
-		InventoryResponse[] inventoryResponses = webClient.get()
-				.uri("http://localhost:8082/api/inventory",
+		InventoryResponse[] inventoryResponses = webClientBuilder.build().get()
+				.uri("http://inventory-service/api/inventory",
 						uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes)
 								.build())
 				.retrieve()
