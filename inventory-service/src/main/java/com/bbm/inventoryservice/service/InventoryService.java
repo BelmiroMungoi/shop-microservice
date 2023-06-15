a4ab6c8ee7f241e549bd6887319d120e18dbd4b7
Belmiro Mungoi <belmiromungoi@gmail.com>
2023-06-15 08:10:26 +0200

    adding a thread for the circuit breaker

--- inventory-service/src/main/java/com/bbm/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/bbm/inventoryservice/service/InventoryService.java
@@ -2,6 +2,8 @@ package com.bbm.inventoryservice.service;
 
 import com.bbm.inventoryservice.model.Inventory;
 import com.bbm.inventoryservice.model.dto.InventoryResponse;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,12 +15,17 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
 	private final InventoryRepository inventoryRepository;
 	
 	@Transactional(readOnly = true)
+	@SneakyThrows
 	public List<InventoryResponse> isInStock(List<String> skuCode) {
+		log.info("Wait Started");
+		Thread.sleep(10000);
+		log.info("Wait Ended");
 		return inventoryRepository.findBySkuCodeIn(skuCode).stream()
 				.map(inventory ->
 						InventoryResponse.builder()
